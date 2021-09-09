0e61acfead03636b2836d943134d6506fbdcf089
zlRivo <58393673+zlRivo@users.noreply.github.com>
2021-09-09 17:29:58 +0200

    Working character controller

---
+++ Scenes/Instances/Player/Player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+# References
+onready var sprite = $Sprite
+
+const JUMP_HEIGHT = 300
+const MAX_FALLSPEED = 400
+const GRAVITY = 2
+
+const MAXSPEED = 160
+const ACCEL = 6
+const DECEL = 6
+
+var motion = Vector2.ZERO
+
+func _physics_process(delta):
+	motion.y = motion.y + GRAVITY
+	if motion.y > MAX_FALLSPEED:
+		motion.y = MAX_FALLSPEED
+	
+	if Input.is_action_just_pressed("jump_0"):
+		if is_on_floor():
+			motion.y = -JUMP_HEIGHT
+	
+	if Input.is_action_pressed("right_0"):
+		motion.x += ACCEL
+		sprite.scale.x = 1
+	elif Input.is_action_pressed("left_0"):
+		motion.x -= ACCEL
+		sprite.scale.x = -1
+	else:
+		motion.x = lerp(motion.x, 0, 0.02)
+	
+	motion.x = clamp(motion.x, -MAXSPEED, MAXSPEED)
+		
+	motion = move_and_slide(motion, Vector2.UP)
--- Scenes/Main/SceneHandler/SceneHandler.gd
+++ Scenes/Main/SceneHandler/SceneHandler.gd
@@ -2,6 +2,8 @@ extends Node
 
 # Menu scene
 # World scenes
+var plain_ground_map_scene = preload("res://Scenes/Main/PlainGroundMap/PlainGroundMap.tscn")
 
 func _ready():
-	pass
+	var plain_ground_map = plain_ground_map_scene.instance()
+	add_child(plain_ground_map)
