141ca572f3f4c4761d318a590fe3a86c2cd8caa1
zlRivo <58393673+zlRivo@users.noreply.github.com>
2021-09-12 10:44:25 +0200

    Added Background Music for each map, polished PlainGround map, added a map randomizer, and started building the SnowyTownMap

--- Scenes/Main/SceneHandler/SceneHandler.gd
+++ Scenes/Main/SceneHandler/SceneHandler.gd
@@ -2,9 +2,22 @@ extends Node
 
 # Menu scene
 # World scenes
-var plain_ground_map_scene = preload("res://Scenes/Maps/PlainGroundMap/PlainGroundMap.tscn")
+var world_scenes = {
+	"PlainGround": preload("res://Scenes/Maps/PlainGroundMap/PlainGroundMap.tscn"),
+	"SnowyTown": preload("res://Scenes/Maps/SnowyTownMap/SnowyTownMap.tscn")
+}
+
 onready var map = $Map
 
 func _ready():
-	var plain_ground_map = plain_ground_map_scene.instance()
-	map.add_child(plain_ground_map)
+	# Set a random map on start
+	map.set_map(get_random_map())
+	
+# Returns a random map from the dictionary
+func get_random_map() -> PackedScene:
+	# Better random
+	randomize()
+	# Get keys of dictionary
+	var world_keys = world_scenes.keys()
+	# Return random map
+	return world_scenes[world_keys[randi() % world_keys.size()]]
